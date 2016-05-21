c163c5fa5b174a0b4e4aec6a3dae79b225a97184
Teenu <teenu@carmatec.com>
2016-05-21 14:00:14 +0530

    Added capacity method to responder model

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -4,5 +4,13 @@ class Responder < ActiveRecord::Base
   belongs_to :emergency
   validates :capacity,:type , presence: true                  
   validates :name, presence: true,uniqueness: true
+ response_types = %w(Fire Medical Police)
+def current_capacity(response_types)
+     hash = {}
+    response_types.each do |type|
+      capacity_hash[type] = Responder.where(type: type)
+      capacity = capacity_hash[type].pluck(:capacity)
+    end
+  end
 
 end
