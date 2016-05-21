fc7c5602cb50266c7026543929f083a0a5e7f1be
Teenu <teenu@carmatec.com>
2016-05-21 13:15:20 +0530

    Modified the responder controller with update action

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -9,10 +9,29 @@ class RespondersController < ApplicationController
     @responder = Responder.new(responder_params(:create))
     @responder.save
   end 
+  
+  def show
+    @responder = Responder.find_by_code!(params[:name])
+  end
 
-  private
+  def update
+    @responder = Responder.find_by_code!(params[:name])
+      if @responder.update_attributes(responder_params_edit)
+        render :show
+      end
+    else
+      redirect_to :back
+    end
+  end
+
+
+private
 
   def responder_params
+    params.require(:responder).permit(:name,:type,:capacity,:emergency_code)
+  end
+
+  def responder_params_edit
     params.require(:responder).permit(:name,:type,:capacity,:on_duty,:emergency_code)
   end
 
